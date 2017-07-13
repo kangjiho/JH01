e4e624796d70476daa930115022502d8020dec6c
kangjiho <jhkang@kaeri.re.kr>
2017-07-13 13:05:26 +0900

    subdir

---
+++ Olds/SAPCOR_v04f_Force_Block_V_F_Build001.py
@@ -0,0 +1,362 @@
+VERSION_NAME = 'SAPCOR_v04f'
+
+# MODULE NAME
+MODULE_NAME_THIS__FORCE_BLOCK_V_F = VERSION_NAME+'_Force_Block_V_F'
+
+
+import glob
+
+# -------------------------------------------------------------------
+# MODULE NAMES TO BE IMPORTED
+MODULE_NAMES_FORCE_BLOCK_V_F = []
+MODULE_NAMES_FORCE_BLOCK_V_F.append('Misc')
+MODULE_NAMES_FORCE_BLOCK_V_F.append('Input')
+# -------------------------------------------------------------------
+
+# -------------------------------------------------------------------
+# Import Modules
+for Module_Name in MODULE_NAMES_FORCE_BLOCK_V_F:
+  FileName = VERSION_NAME+'_'+Module_Name+'_Build*.py'
+  FList = glob.glob(FileName)
+  assert len(FList)!=0,'ERROR: Module not found : '+FileName
+  FList.sort()
+  FileName = FList[-1][0:-3]
+  exec('from '+FileName+' import *')
+# -------------------------------------------------------------------
+
+
+from numpy import loadtxt
+from numpy import array,zeros,hstack
+
+
+
+#-------------------------------------------------------------------------
+
+def Force_Block_V_F (w,t,Core,K,L,Accel):
+  
+  """
+    Defines the differential equations for the coupled spring-mass system.
+
+    Arguments:
+        w :  vector of the state variables at time, t:
+                  w = [U,DU,W,DW,R,DR,...]
+        t :  time
+        Core : Var space
+  """
+
+  # VERBOSE
+  if '/Force/' in VERBOSE:
+    Verbose('<FORCE_BLOCK_V>')
+    Verbose('time=%e, (K,L)='%(t,K,L))
+    
+
+  # Column Size
+  N = Core['N'][K]
+
+  # Get Block Prop of (K,L)
+#  BN = Core['Array'][K][L]
+  BN = Core['BTNsKL'][K][L]
+  BT = BlockTypes[BN]
+  A = BT['a']
+  H = BT['h']
+  M = BT['M']
+  I = BT['I']
+  Kv = BT['Kv']
+  Cv = BT['Cv']
+  mu_s = BT['mu_s']
+  mu_k = BT['mu_k']
+  d_mu = BT['d_mu']
+  xi_F_cr = BT['xi_F_cr']
+#  Fixed = BT['Fixed']
+
+  # Get Block Prop of (K,L-1)
+  # Subscript, m, means 'Minus 1'
+  if L!=1:
+#    BN = Core['Array'][K][L-1]
+    BN = Core['BTNsKL'][K][L-1]
+    BT = BlockTypes[BN]
+#    Am = BT['a']
+    Hm = BT['h']
+    Mm = BT['M']
+    Im = BT['I']
+#    Kvm = BT['Kv']
+#    Cvm = BT['Cv']
+#    Fixedm = BT['Fixed']
+  else:
+    if Core['Flag_CSB']==True: # CSB
+      BT = BlockTypes['CSB']
+      Hm = 0
+      Mm = BT['M']
+      Im = BT['I']
+    else: # Base
+      Hm = 0
+      Mm = Im = 1
+      
+  
+
+  # Get current state of (K,L)
+  U,DU,W,DW,R,DR = GetSolFromVect(Core,w,K,L) # U,DU,W,DW,R,DR
+
+  # Get current state of (K,L-1)
+  # If L==1:
+  #   It means lower component is CSB or Base.
+  #   If Flag_CSB==True: Get state of CSB  (which means (K,L)==(1,0))
+  #   Else             : Get state of Base (which means (K,L)==(0,0))
+  if L!=1:
+    Um,DUm,Wm,DWm,Rm,DRm = GetSolFromVect(Core,w,K,L-1) # U,DU,W,DW,R,DR
+  else:
+    if Core['Flag_CSB']==True:
+      Um,DUm,Wm,DWm,Rm,DRm = GetSolFromVect(Core,w,1,0) # CSB
+    else:
+      Um,DUm,Wm,DWm,Rm,DRm = GetSolFromVect(Core,w,0,0) # Base
+    
+
+
+##############################################################################
+#  VERTICAL FORCES
+##############################################################################
+
+  if Core['ApplyForces']['Block_V']==True:
+
+    # Spring Contraction between (K,L-1)~(K,L)
+    if L!=1:
+      Gamma_L  = Wm - Hm * (1-cos( Rm )) + A  * sin( Rm )
+      Gamma_L -= W  + H  * (1-cos( R  )) + A  * sin( R  )
+      Gamma_L *= 0.5
+      Gamma_R  = Wm - Hm * (1-cos( Rm )) - A  * sin( Rm )
+      Gamma_R -= W  + H  * (1-cos( R  )) - A  * sin( R  )
+      Gamma_R *= 0.5
+  
+      DGamma_L  = DWm - ( Hm * sin( Rm ) - A  * cos( Rm ) ) * DRm
+      DGamma_L -= DW  + ( H  * sin( R  ) + A  * cos( R  ) ) * DR
+      DGamma_L *= 0.5
+      DGamma_R  = DWm - ( Hm * sin( Rm ) + A  * cos( Rm ) ) * DRm
+      DGamma_R -= DW  + ( H  * sin( R  ) - A  * cos( R  ) ) * DR
+      DGamma_R *= 0.5
+    else: # Interaction between CSB(or Base)~(K,1)
+      Gamma_L  = Wm
+      Gamma_L -= W  + H  * (1-cos( R  )) + A  * sin( R  )
+      Gamma_L *= 0.5
+      Gamma_R  = Wm
+      Gamma_R -= W  + H  * (1-cos( R  )) - A  * sin( R  )
+      Gamma_R *= 0.5
+  
+      DGamma_L  = DWm
+      DGamma_L -= DW  + ( H  * sin( R  ) + A  * cos( R  ) ) * DR
+      DGamma_L *= 0.5
+      DGamma_R  = DWm
+      DGamma_R -= DW  + ( H  * sin( R  ) - A  * cos( R  ) ) * DR
+      DGamma_R *= 0.5
+  
+    
+    # Forces and Moments
+    F_VL,   F_VR = 0,0
+    M_VL, M_VR, M_VLm, M_VRm = 0,0,0,0
+   
+    if Gamma_L>0:
+      F_VL  =  Kv * Gamma_L  + Cv * DGamma_L
+    if Gamma_R>0:
+      F_VR  =  Kv * Gamma_R  + Cv * DGamma_R
+  
+    M_VL  =  F_VL * ( A*cos(R ) + H *sin(R ) )
+    M_VR  = -F_VR * ( A*cos(R ) - H *sin(R ) )
+    M_VLm = -F_VL * ( A*cos(Rm) - Hm*sin(Rm) )
+    M_VRm =  F_VR * ( A*cos(Rm) + Hm*sin(Rm) )
+  
+    
+    #---------------------------
+    # ELIMINATE STICKING FORCE
+    #---------------------------
+    if FLAG_NoStickForce == True:
+      if M_VL  < 0. : M_VL  = 0.
+      if M_VR  < 0. : M_VR  = 0.
+      if M_VLm > 0. : M_VLm = 0.
+      if M_VRm > 0. : M_VRm = 0.
+    #---------------------------
+  
+  
+    # Assemble Forces
+    AccelF  =  (F_VL  + F_VR ) / M
+    AccelM  =  (M_VL  + M_VR ) / I
+    AccelFm = -(F_VL  + F_VR ) / Mm
+    AccelMm =  (M_VLm + M_VRm) / Im
+    
+    # Add Accel on (K,L)
+    IndexW = Core['Index'][K][L]*6
+    Accel[IndexW+3] += AccelF # DDW
+    Accel[IndexW+5] += AccelM # DDR
+  
+    # Add Accel on (K,L-1)
+    
+    if L!=1: # Normal Blocks
+      IndexWm = Core['Index'][K][L-1]*6
+      Accel[IndexWm+3] += AccelFm # DDW
+      Accel[IndexWm+5] += AccelMm # DDR
+   
+    elif Core['Flag_CSB']==True: # Add Accel on CSB
+      pass # if CSB: Do nothing
+  
+    pass # if Base: Do nothing
+  
+  
+    # VERBOSE
+    if '/ForceV/' in VERBOSE:
+      Verbose('-'*80)
+      Verbose('Force_Block_V(), Caculation Verification')
+      Verbose('Block (%d,%d)'%(K,L))
+      Verbose('U, DU, W, DW, R, DR  = %e, %e, %e, %e, %e, %e'%(U,DU,W,DW,R,DR))
+      Verbose('Um,DUm,Wm,DWm,Rm,DRm = %e, %e, %e, %e, %e, %e'%(Um,DUm,Wm,DWm,Rm,DRm))
+      Verbose('Gamma  = %e, %e'%(Gamma_L,Gamma_R))
+      Verbose('DGamma = %e, %e'%(DGamma_L,DGamma_R))
+      Verbose('FV  = %e, %e'%(F_VL,F_VR))
+      Verbose('MFV = %e, %e, %e, %e'%(M_VL,M_VR,M_VLm,M_VRm))
+      Verbose('Accel(%d,%d) = %e, %e'%(K,L,AccelF,AccelM))
+      Verbose('Accel[%d] = %e'%(IndexW+3,Accel[IndexW+3]))
+      Verbose('Accel[%d] = %e'%(IndexW+5,Accel[IndexW+5]))
+      if L!=1:
+        Verbose('Accel(%d,%d) = %e, %e'%(K,L-1,AccelFm,AccelMm))
+        Verbose('Accel[%d] = %e'%(IndexWm+3,Accel[IndexWm+3]))
+        Verbose('Accel[%d] = %e'%(IndexWm+5,Accel[IndexWm+5]))
+
+
+
+
+
+
+##############################################################################
+#  FRICTION FORCES
+##############################################################################
+
+  if Core['ApplyForces']['Block_F']==True:
+
+    # Init
+    xi_L=xi_R=0
+    F_FL=F_FLm=M_FL=M_FLm=F_FR=F_FRm=M_FR=M_FRm=0
+    
+    #===========================================================================
+    # LEFT start
+    
+    # Vertical Reaction should be positive
+    if F_VL>0: 
+      
+      # Relative Velocity
+      xi_L    = DU  - ( H  * cos( R  ) - A * sin( R  ) ) * DR
+      if L!=1:
+        xi_L -= DUm + ( Hm * cos( Rm ) + A * sin( Rm ) ) * DRm
+      else:
+        xi_L -= DUm
+      
+      # Relative Velocity should be nonzero
+      if xi_L!=0:
+        
+        # Friction Force: F_FL      
+        if abs(xi_L) <= xi_F_cr:
+          # Viscous Slip Condition
+          F_FL = - mu_s * F_VL * xi_L / xi_F_cr
+        else:
+          # Slip Condition
+          mu = mu_k + (mu_s - mu_k) * exp( -d_mu*(abs(xi_L)-xi_F_cr) )
+          F_FL = - (+(xi_L>0) or -(xi_L<0)) * mu * F_VL
+          
+        # Other Forces & Moments: F_FLm, M_FL, M_FLm
+        F_FLm = -F_FL
+        M_FL    = -F_FL  * ( H  * cos( R  ) - A * sin( R  ) )
+        if L!=1:
+          M_FLm =  F_FLm * ( Hm * cos( Rm ) + A * sin( Rm ) )
+        
+      # Check Point: xi==0 -> Do nothing
+        
+    # Check Point: F_VL<=0 -> Do nothing
+    
+    
+    # LEFT end
+    #===========================================================================
+    
+  
+    #===========================================================================
+    # RIGHT start
+    
+    # Vertical Reaction should be positive
+    if F_VR>0: 
+      
+      # Relative Velocity
+      xi_R    = DU  - ( H  * cos( R  ) + A * sin( R  ) ) * DR
+      if L!=1:
+        xi_R -= DUm + ( Hm * cos( Rm ) - A * sin( Rm ) ) * DRm
+      else:
+        xi_R -= DUm
+      
+      # Relative Velocity should be nonzero
+      if xi_R!=0:
+        
+        # Friction Force: F_FR      
+        if abs(xi_R) <= xi_F_cr:
+          # Viscous Slip Condition
+          F_FR = - mu_s * F_VR * xi_R / xi_F_cr
+        else:
+          # Slip Condition
+          mu = mu_k + (mu_s - mu_k) * exp( -d_mu*(abs(xi_R)-xi_F_cr) )
+          F_FR = - (+(xi_R>0) or -(xi_R<0)) * mu * F_VR
+        
+        # Other Forces & Moments: F_FRm, M_FR, M_FRm
+        F_FRm = -F_FR
+        M_FR    = -F_FR  * ( H  * cos( R  ) + A * sin( R  ) )
+        if L!=1:
+          M_FRm =  F_FRm * ( Hm * cos( Rm ) - A * sin( Rm ) )
+        
+      # Check Point: xi==0 -> Do nothing
+        
+    # Check Point: F_VR<=0 -> Do nothing
+    
+    
+    # RIGHT end
+    #===========================================================================
+  
+    # Assemble Accel
+    AccelF  =  (F_FL  + F_FR ) / M
+    AccelM  =  (M_FL  + M_FR ) / I
+    AccelFm = -(F_FL  + F_FR ) / Mm
+    AccelMm =  (M_FLm + M_FRm) / Im
+    
+    # Add Accel on (K,L)
+    IndexW = Core['Index'][K][L]*6
+    Accel[IndexW+1] += AccelF # DDU
+    Accel[IndexW+5] += AccelM # DDR
+  
+    # Add Accel on (K,L-1)
+    
+    if L!=1: # Normal Blocks
+      IndexWm = Core['Index'][K][L-1]*6
+      Accel[IndexWm+1] += AccelFm # DDU
+      Accel[IndexWm+5] += AccelMm # DDR
+   
+    elif Core['Flag_CSB']==True: # Add Accel on CSB
+      IndexWm = Core['Index'][K][L-1]*6
+      Accel[IndexWm+1] += AccelFm # DDU
+      pass # No moment on CSB
+  
+    pass # if Base: Do nothing
+
+
+    # VERBOSE
+    if '/ForceF/' in VERBOSE:
+      Verbose('-'*80)
+      Verbose('Force_Block_F(), Caculation Verification')
+      Verbose('Block (%d,%d)'%(K,L))
+      Verbose('U, DU, W, DW, R, DR  = %e, %e, %e, %e, %e, %e'%(U,DU,W,DW,R,DR))
+      Verbose('Um,DUm,Wm,DWm,Rm,DRm = %e, %e, %e, %e, %e, %e'%(Um,DUm,Wm,DWm,Rm,DRm))
+      Verbose('xi  = %e, %e'%(xi_L,xi_R))
+      Verbose('FF  = %e, %e'%(F_FL,F_FR))
+      Verbose('MF  = %e, %e, %e, %e'%(M_FL,M_FR,M_FLm,M_FRm))
+      Verbose('Accel(%d,%d) = %e, %e'%(K,L,AccelF,AccelM))
+      Verbose('Accel[%d] = %e'%(IndexW+1,Accel[IndexW+1]))
+      Verbose('Accel[%d] = %e'%(IndexW+5,Accel[IndexW+5]))
+      if L!=1:
+        Verbose('Accel(%d,%d) = %e, %e'%(K,L-1,AccelFm,AccelMm))
+        Verbose('Accel[%d] = %e'%(IndexWm+1,Accel[IndexWm+1]))
+        Verbose('Accel[%d] = %e'%(IndexWm+5,Accel[IndexWm+5]))
+
+
+
+  # END
+  return Accel
---
+++ Olds/SAPCOR_v04f_Force_Build001.py
@@ -0,0 +1,577 @@
+VERSION_NAME = 'SAPCOR_v04f'
+
+# MODULE NAME
+MODULE_NAME_THIS__FORCE = VERSION_NAME+'_Force'
+
+
+import glob
+
+# -------------------------------------------------------------------
+# MODULE NAMES TO BE IMPORTED
+MODULE_NAMES_FORCE = []
+MODULE_NAMES_FORCE.append('Misc')
+MODULE_NAMES_FORCE.append('Input')
+MODULE_NAMES_FORCE.append('Force_Block_D_F')
+MODULE_NAMES_FORCE.append('Force_Block_H')
+#MODULE_NAMES_FORCE.append('Force_Block_V')
+MODULE_NAMES_FORCE.append('Force_Block_V_F')
+# -------------------------------------------------------------------
+
+# -------------------------------------------------------------------
+# Import Modules
+for Module_Name in MODULE_NAMES_FORCE:
+  FileName = VERSION_NAME+'_'+Module_Name+'_Build*.py'
+  FList = glob.glob(FileName)
+  assert len(FList)!=0,'ERROR: Module not found : '+FileName
+  FList.sort()
+  FileName = FList[-1][0:-3]
+  exec('from '+FileName+' import *')
+# -------------------------------------------------------------------
+
+
+
+
+
+
+from numpy import loadtxt
+from numpy import array,zeros,hstack
+import time
+
+
+
+# PERFORMANCE MEASUREMENT
+# VectorField()
+NRun_VectorField = 0
+RunTime_VectorField = 0.
+NRun_VectorField_Prev = 0
+RunTime_VectorField_Prev = 0.
+
+
+
+
+#=========================================================================
+# VECTOR FIELD
+#
+#
+#
+def VectorField(w, t, Core, VERBOSE=''):
+  """
+    Defines the differential equations for the coupled spring-mass system.
+
+    Arguments:
+        w :  vector of the state variables at time, t:
+                  w = [U,DU,W,DW,R,DR,...]
+        t :  time
+        Core : Var space
+    
+    Return:
+        [DU,DDU,DW,DDW,DR,DDR,...]
+  """
+
+  # PERFORMANCE MEASUREMENT
+  # Globa Vars:
+  #   NRun_VectorField : number of runs of VectorField()
+  #   AvgRunTime_VectorField : Average Run Time of VectorField()
+  global NRun_VectorField
+  global RunTime_VectorField
+  StartTime=time.time()
+
+# VERBOSE    
+  if '/Force/' in VERBOSE:
+    Verbose('='*80)
+    Verbose('<VectorField()>')
+
+
+  # Var Space
+
+#  CoreIndex = Core['Index']
+  LCoreReverseIndex = len(Core['ReverseIndex'])
+  M2 = Core['M']+2 # 0: Left restraint, 1~(M-1): Blocks, M+1: Right restraint
+  BodyForce=Core['BodyForce']
+  
+  # InitTemp Memory
+  Accel = [0]*(LCoreReverseIndex*6)
+
+  # Index for CoreIndex
+  IndexBlock = 0
+
+  # Index for Solution Vector
+  # IndexW = IndexBlock*6
+  IndexW = 0
+
+
+  
+  # ------------------------------------------------------------------------
+  # COPY STATE OF BASE TO "FIXED TO BASE" BLOCKS
+  #
+  StateOfBase=GetSolFromVect(Core,w,0,0)
+  for (K,L) in Core['KLFixedToBase']:
+    PutValToVect(Core,w,K,L,StateOfBase)
+
+  
+  # ------------------------------------------------------------------------
+  # MAIN LOOP FOR CORE
+  #
+  for IndexBlock in xrange(LCoreReverseIndex):
+
+
+
+
+  
+    # Index on StateVector
+    IndexW = IndexBlock*6    
+    
+    # Get (K,L) index from sequential index list
+    K,L = Core['ReverseIndex'][IndexBlock]
+    BTN = Core['BTNs'][IndexBlock]
+    
+    # Read 'Fixed' Setting
+    if (K,L)==(0,0): # Base
+      Fixed = 'FixedToBase'
+    else: # CSB or normal blocks
+      Fixed = BlockTypes[BTN]['Fixed']
+
+
+
+
+
+# VERBOSE    
+    if '/Force/' in VERBOSE:
+      Verbose('IndexBlock,IndexW=%d,%d  K,L=%d,%d'%(IndexBlock,IndexW,K,L))
+
+
+
+    
+    
+    
+    # ------------------------------------------------------------------------
+    # Force on Base
+    if [K,L]==[0,0]: Force_Base(w,t,Core)
+
+
+
+
+
+    # ------------------------------------------------------------------------
+    # FORCE ON CSB
+    #
+    elif [K,L]==[1,0]: Force_CSB(w,t,Core)
+
+      # DO FOLLOWIN IN Force_CSB()
+#      if Fixed=='None': Force_CSB(w,t,Core,f)
+
+      # STATE AND FORCE UPDATES ARE DETERMINED BY 'Fix' SETTING
+      # WHICH WAS SET IN BlockTypes IN INPUT FILE
+
+      # If not fixed -> Calculate Accel
+#      if Fixed=='None': Accel=Force_CSB(w,t,Core)
+
+      # If 'Fixed' -> Make Accel={0} (means maintaining initial state)
+#      elif Fixed=='Fixed': Accel=[0.,0.,0.]
+        
+      # If 'FixedToBase' -> Copy State of Base to current state
+      #                     And make Accel={0}
+#      elif Fixed=='FixedToBase':
+#        StateOfBase=GetSolFromVect(Core,w,0,0)
+#        PutValToVect(Core,w,K,L,StateOfBase)
+#        Accel=[0.,0.,0.]
+
+      # INPUT ERROR
+#      else: ERROR('Invalid CSB Fix Setting')
+    #
+    # END OF FORCE ON CSB
+    # ------------------------------------------------------------------------
+
+
+    # ------------------------------------------------------------------------
+    # INVAILD K,L
+    elif K>1 and L==0: ERROR('Invalid Core Index')
+    # END OF INVAILD K,L
+    # ------------------------------------------------------------------------
+
+
+
+    
+
+
+
+
+
+    # ------------------------------------------------------------------------
+    # FORCE ON NORMAL BLOCKS
+    #
+    else: # Blocks
+
+
+        if '/Force/' in VERBOSE:
+          Verbose('Call FORCE_BLOCK')
+
+        # FORCE ON LEFT REFLECTORS
+        if K==0: Force_ReflectorL(w,t,Core,K,L)
+    
+        # FORCE ON RIGHT REFLECTORS
+        elif K==Core['M']+1: Force_ReflectorR(w,t,Core,K,L)
+
+        # FORCE ON INNER BLOCKS
+        else:
+#          Accel=Force_Block(w,t,Core,K,L,Accel)
+          
+          # HORIZONTAL
+          if Core['ApplyForces']['Block_H']==True:
+            Accel = Force_Block_H(w,t,Core,K,L,Accel)
+          
+        
+          # VERTICAL & FRICTION
+#          if Core['ApplyForces']['Block_V']==True:
+#            Accel = Force_Block_V(w,t,Core,K,L,Accel) 
+          Accel = Force_Block_V_F(w,t,Core,K,L,Accel) 
+          
+          # DOWEL
+          if Core['ApplyForces']['Block_D']==True:
+            Accel = Force_Block_D_F(w,t,Core,K,L,Accel) 
+
+
+      # DO FOLLOWING IN SEPARATE FORCE FUNCTIONS        
+      # STATE AND FORCE UPDATES ARE DETERMINED BY 'Fix' SETTING
+      # WHICH WAS SET IN BlockTypes IN INPUT FILE
+
+      # If not fixed -> Calculate Accel
+#      if Fixed=='None':
+
+
+        # FORCE ON LEFT REFLECTORS
+#        if K==0: # Left Restraint
+#          Accel=Force_RestraintL(w,t,Core,K,L)
+    
+        # FORCE ON RIGHT REFLECTORS
+#        elif K==Core['M']+1: # Right Restraint
+#          Accel=Force_RestraintR(w,t,Core,K,L)
+
+        # FORCE ON INNER BLOCKS
+#        else:
+#          Accel=Force_Block(w,t,Core,K,L)
+
+      # If 'Fixed' -> Make Accel={0] (means maintaining initial state)
+#      elif Fixed=='Fixed': Accel=[0.,0.,0.]
+        
+      # If 'FixedToBase' -> Copy State of Base to current state
+      #                     And make Accel={0}
+#      elif Fixed=='FixedToBase':
+#        StateOfBase=GetSolFromVect(Core,w,0,0)
+#        PutValToVect(Core,w,K,L,StateOfBase)
+#        Accel=[0.,0.,0.]
+        
+      # INPUT ERROR
+#      else: ERROR('Invalid CSB Fix Setting')
+    #
+    # END OF FORCE ON BLOCKS
+    # ------------------------------------------------------------------------
+
+        
+
+
+
+
+    # ------------------------------------------------------------------------
+    # BODY FORCE
+    #
+    # Cancel->Body Force (1:DDU, 2:DDV, 3:DDW, 4:DDQ, 5:DDR, 6:DDS)
+    # Body Force (0:DU 1:DDU, 2:DW, 3:DDW, 4:DR, 5:DDR)
+    # If BASE -> No Body Force
+    if [K,L]!=[0,0] and [K,L]!=[1,0] and K!=0 and K!=L+1 and Fixed=='None':
+      # DO SOMETHING HERE
+      Accel[IndexW+1] += BodyForce[1] # DDU
+      Accel[IndexW+3] += BodyForce[3] # DDW
+      Accel[IndexW+5] += BodyForce[5] # DDR
+      pass
+    #     
+    # END OF BODY FORCE
+    # ------------------------------------------------------------------------
+    
+
+
+
+   
+    # ------------------------------------------------------------------------
+    # ASSEMBLE FORCE
+    #
+    
+    Accel[IndexW  ] = w[IndexW+1] # DU
+    Accel[IndexW+2] = w[IndexW+3] # DW
+    Accel[IndexW+4] = w[IndexW+5] # DR
+  
+
+    # Manage Fixed
+    if Fixed=='Fixed':
+      Accel[IndexW  ] = w[IndexW+1] # DU
+      Accel[IndexW+1] = 0 # DDU
+      Accel[IndexW+2] = w[IndexW+3] # DW
+      Accel[IndexW+3] = 0 # DDW
+      Accel[IndexW+4] = w[IndexW+5] # DR
+      Accel[IndexW+5] = 0 # DDR      
+    elif Fixed=='FixedToBase':
+      Accel[IndexW  ] = 0 # DU
+      Accel[IndexW+1] = 0 # DDU
+      Accel[IndexW+2] = 0 # DW
+      Accel[IndexW+3] = 0 # DDW
+      Accel[IndexW+4] = 0 # DR
+      Accel[IndexW+5] = 0 # DDR      
+    elif Fixed=='FixedU':
+      Accel[IndexW  ] = 0 #  DU=0 -> next step:  U=Contant
+      Accel[IndexW+1] = 0 # DDU=0 -> next step: DU=Constant
+#      Accel[IndexW+2] = 0 #  DW=0 -> next step:  W=Constant
+#      Accel[IndexW+3] = 0 # DDW=0 -> next step: DW=Constant 
+#      Accel[IndexW+4] = 0 #  DR=0 -> next step:  R=Constant
+#      Accel[IndexW+5] = 0 # DDR=0 -> next step: DR=Constant      
+    elif Fixed=='FixedUR':
+      Accel[IndexW  ] = 0 #  DU=0 -> next step:  U=Contant
+      Accel[IndexW+1] = 0 # DDU=0 -> next step: DU=Constant
+#      Accel[IndexW+2] = 0 #  DW=0 -> next step:  W=Constant
+#      Accel[IndexW+3] = 0 # DDW=0 -> next step: DW=Constant 
+      Accel[IndexW+4] = 0 #  DR=0 -> next step:  R=Constant
+      Accel[IndexW+5] = 0 # DDR=0 -> next step: DR=Constant      
+
+
+#    if '/Force/' in VERBOSE:
+#      Verbose('-'*80)
+#      Verbose('VerctorField(), Force Assembly Result')
+#      Verbose('Block (%d,%d) : Force = %s'%(K,L,Force))
+#    f = hstack([f,Force])
+
+    #    
+    # END OF ASSEMBLE FORCE
+    # ------------------------------------------------------------------------
+
+
+
+
+#    IndexW += 6
+  #
+  # END OF MAIN LOOP FOR CORE
+  # ------------------------------------------------------------------------
+
+
+
+
+# TEST #
+#  if Core['TEST']!=Core['InitialBlockCenters']:
+#    print "Force-Loop2 Core['InitialBlockCenters']"
+#    for K in Core['InitialBlockCenters'].keys():
+#      Col = Core['InitialBlockCenters'][K]
+#      for L in Col.keys():
+#        print '(%d,%d)'%(K,L),'=',Col[L]
+#    raw_input('pause')
+
+  
+
+
+
+
+  # PERFORMANCE MEASUREMENT
+  RunTime_VectorField += time.time()-StartTime
+  NRun_VectorField += 1
+
+  return Accel
+#
+#
+#
+#
+# END OF VECTOR FIELD
+#=========================================================================
+
+
+
+
+
+
+def PerfResult_VectorField():
+  """
+Return: (a,b,c,d,e)
+    a: NRun_VectorField    of this section only
+    b: RunTime_VectorField of this section only
+    c: Average RunTime     of this section only
+    d: NRun_VectorField    of all calculation from beginning
+    e: RunTime_VectorField of all calculation from beginning
+    f: Average RunTime     of all calculation from beginning
+  """
+  global NRun_VectorField
+  global RunTime_VectorField
+  global NRun_VectorField_Prev
+  global RunTime_VectorField_Prev
+  a = NRun_VectorField    - NRun_VectorField_Prev
+  b = RunTime_VectorField - RunTime_VectorField_Prev
+  c = b / a  
+  d = NRun_VectorField
+  e = RunTime_VectorField
+  f = RunTime_VectorField / NRun_VectorField
+  NRun_VectorField_Prev    = NRun_VectorField
+  RunTime_VectorField_Prev = RunTime_VectorField
+  return a,b,c,d,e,f 
+
+
+
+
+
+#=========================================================================
+# FIXED TO BASE
+#=========================================================================
+#def CopyBaseToFixedToBase (w,t,Core):
+#  StateOfBase=GetSolFromVect(Core,w,0,0)
+#  for (K,L) in Core['KLFixedToBase']:
+#    PutValToVect(Core,w,K,L,StateOfBase)
+#  return
+
+#def AccelModForFixedBlocks (w,t,Core):
+#  global Accel
+# 
+#  # Fixed
+#  for (K,L) in Core['KLFixed']:
+#    (U,DU,W,DW,R,DR) = GetSolFromVect(Core,w,K,L)
+#    Accel_Bl = [DU,0.,DW,0.,DR,0.]
+#    PutValToVect(Core,Accel,K,L,Accel_Bl)
+#    
+#  # FixedToBase
+#  for (K,L) in Core['KLFixedToBase']:
+#    Accel_Bl = [0.,0.,0.,0.,0.,0.]
+#    PutValToVect(Core,Accel,K,L,Accel_Bl)
+#    
+#  return  
+  
+  
+#=========================================================================
+# BLOCK FORCES
+#=========================================================================
+
+
+
+
+#-------------------------------------------------------------------------
+
+def Force_Base (w,t,Core):
+  return
+
+#-------------------------------------------------------------------------
+
+def Force_CSB (w,t,Core):
+  return
+
+#-------------------------------------------------------------------------
+
+
+def Force_ReflectorL (w,t,Core,K,L):
+  return
+
+#-------------------------------------------------------------------------
+
+def Force_ReflectorR (w,t,Core,K,L):
+  return
+
+#-------------------------------------------------------------------------
+
+def OLD_Force_Block (w,t,Core,K,L,Accel):
+  
+  # HORIZONTAL
+  if Core['ApplyForces']['Block_H']==True:
+    if '/Force/' in VERBOSE:
+      Verbose('-'*80)
+      Verbose('Call FORCE_BLOCK_H')
+    Accel = Force_Block_H(w,t,Core,K,L)
+  
+
+  # VERTICAL & FRICTION
+#  if Core['ApplyForces']['Block_V']==True:
+  if Core['ApplyForces']['Block_V_F']==True:
+    if '/Force/' in VERBOSE:
+      Verbose('-'*80)
+#      Verbose('Call FORCE_BLOCK_V')
+      Verbose('Call FORCE_BLOCK_V_F')
+#    Accel = Force_Block_V(w,t,Core,K,L,Accel) 
+    Accel = Force_Block_V_F(w,t,Core,K,L,Accel) 
+  
+  # DOWEL
+  if Core['ApplyForces']['Block_D']==True:
+    if '/Force/' in VERBOSE:
+      Verbose('-'*80)
+      Verbose('Call FORCE_BLOCK_D')
+    Accel = Force_Block_D(w,t,Core,K,L,Accel) 
+
+  return Accel
+  
+
+#=========================================================================
+# END OF BLOCK FORCES
+#=========================================================================
+
+
+
+
+
+
+
+
+
+
+
+#=========================================================================
+# Extract Acceleration from State Vector
+#=========================================================================
+
+def OLD_ExtractAccelFromXV (Solution,t,Core,VERBOSE=''):
+  Accel = []
+#  for sol,t_i in Solution,t:
+  for i in range(len(t)):
+    sol = Solution[i]
+    t_i = t[i]
+    
+    # f = [ DX,DDX,DW,DDW,DR,DDR of Base,
+    #       DX,DDX,DW,DDW,DR,DDR of CSB,
+    #       DX,DDX,DW,DDW,DR,DDR of Left Restraint,
+    #       DX,DDX,DW,DDW,DR,DDR of First Block,
+    #       ... ] at t_i
+    f=VectorField(sol,t_i,Core,VERBOSE)
+    
+    # acceleration = [ DDX, DDW, DDR,  ... ]
+    #              = [ f[1],f[3],f[5], ... ] at t_i
+    # Accel.append (acceleration)
+    Accel.append(f[1::2])
+#    Accel=numpy.array(Accel)
+  return Accel
+
+
+def ExtractAccelFromXV (Solution,t,Core,VERBOSE=''):
+
+  start = time.time()
+  
+  Accel = zeros([len(t),len(Core['ReverseIndex'])*3])
+  
+  end = time.time()
+  print '1:',end-start
+  start = end
+
+  print 'len(t)=',len(t)  
+  
+#  for sol,t_i in Solution,t:
+  for i in range(len(t)):
+    sol = Solution[i]
+    t_i = t[i]
+
+    # f = [ DX,DDX,DW,DDW,DR,DDR of Base,
+    #       DX,DDX,DW,DDW,DR,DDR of CSB,
+    #       DX,DDX,DW,DDW,DR,DDR of Left Restraint,
+    #       DX,DDX,DW,DDW,DR,DDR of First Block,
+    #       ... ] at t_i
+    f=VectorField(sol,t_i,Core,VERBOSE)
+    
+    # acceleration = [ DDX, DDW, DDR,  ... ]
+    #              = [ f[1],f[3],f[5], ... ] at t_i
+    # Accel.append (acceleration)
+#    Accel.append(f[1::2])
+
+    Accel[i]=f[1::2]
+    
+  end = time.time()
+  print '4:',end-start
+  start = end
+
+  return Accel
+
